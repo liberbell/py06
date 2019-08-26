2a9bfe69fb2f99fb5b3e339eb9542ed766f04721
Liberbell <liberbell@gmail.com>
2019-08-27 08:10:34 +0900

    0204cleaning7

--- 0204cleaning.py
+++ 0204cleaning.py
@@ -12,5 +12,5 @@ pp.plot(billboard.loc[0, 'x1st.week':'x67th.week'])
 pp.savefig('foo.png')
 
 for index, row in billboard.iterrows():
-    pp.plot(billboard.loc[0, 'x1st.week': 'x76th.week'], color='C0', alpha=0.1)
+    pp.plot(range(1,77), row['x1st.week': 'x76th.week'], color='C0', alpha=0.1)
     pp.savefig('foo2.png')
