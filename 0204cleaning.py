b289bdf060b979cdafa8b8f64ea7ee8b0e686520
Liberbell <liberbell@gmail.com>
2019-08-27 08:09:16 +0900

    0204cleaning6

--- 0204cleaning.py
+++ 0204cleaning.py
@@ -8,5 +8,9 @@ billboard = pd.read_csv('billboard.csv', encoding='latin-1')
 print(billboard.head())
 
 print(billboard.columns)
-pp.plot(range(1,77),billboard.loc[0, 'x1st.week':'x67th.week'])
+pp.plot(billboard.loc[0, 'x1st.week':'x67th.week'])
 pp.savefig('foo.png')
+
+for index, row in billboard.iterrows():
+    pp.plot(billboard.loc[0, 'x1st.week': 'x76th.week'], color='C0', alpha=0.1)
+    pp.savefig('foo2.png')
